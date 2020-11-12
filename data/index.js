3b264eb5e54204969d2acd4a4802664745a708f7
SmitaRath <smita.main@gmail.com>
2020-11-11 19:29:28 -0500

    users collection basic skeleton db routes changes

--- data/index.js
+++ data/index.js
@@ -1,5 +1,9 @@
 const rentingInfoData = require('./rentingInfo');
+const usersData = require("./users");
+const validateData = require("./validate");
 
 module.exports = {
     rentingInfo: rentingInfoData,
+    users:usersData,
+    validate:validateData
 };
