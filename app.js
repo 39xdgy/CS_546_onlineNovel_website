ff642b02260dc227d9f8c2c5f8dc9d8a80066791
39xdgy <39xdgy@gmail.com>
2020-12-13 15:12:04 -0500

    hot fix on pastrented cars

--- app.js
+++ app.js
@@ -41,11 +41,7 @@ app.use("/", async(req,res,next)=>{
     && req.originalUrl!="/"
     && req.originalUrl!="/home/search"
     && req.originalUrl!="/cars/createCar"
-<<<<<<< HEAD
-    && req.originalUrl!="/cars/profile/:id"
-=======
     && req.originalUrl!="/cars/carProfile/:id"
->>>>>>> d8b428e14083de8feebea37b43120d7afad78248
     && req.originalUrl!="/rentingInfo/test"
   )
   {
